a56b69db6a143eedadb6f28dda805455cccbbd7e
HolidayGuy21 <14449@coderacademy.edu.au>
2023-09-27 21:03:16 +1000

    intial commitment

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import Home from './Home.jsx'
+import Menu from './Menu.jsx'
+import About from './About.jsx'
+import Contact from './Contact.jsx'
+import NavBar from './NavBar'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<h3>Error 404 - Page not found</h3>}/>
+        </Routes>
+    </>
+  );
+}
+
+export default App;
